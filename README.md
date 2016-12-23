3ce4702638a90d8c7fb6236bba8035b57a89882f
Owlypixel <Y.Aleskerov@foss.kharkov.ua>
2016-12-23 22:14:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_puzzles
+A collection of different python puzzles and solutions
---
+++ solutions.py
@@ -0,0 +1,11 @@
+# find two numbers that are closest to each other among a list of numbers and print min difference. 
+
+a = [3, 5, 8, 9]
+
+b = sorted(a)
+minDiff = abs(b[1] - b[0])
+count = 0
+while count < len(b) - 1:
+    minDiff = min(minDiff, abs(b[count] - b[count + 1]))
+    count += 1
+print(minDiff)
