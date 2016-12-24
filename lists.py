5f28abaa4a7e992914853ef7343ca5c11397e482
Owlypixel <Y.Aleskerov@foss.kharkov.ua>
2016-12-24 03:50:55 +0200

    updating lists list

--- lists.py
+++ lists.py
@@ -12,3 +12,24 @@
 
 # a = [1, 4, 6, 2, 0, -3]
 # print(a[-3:])
+
+# Find the k'th element of a list
+
+# k = 5
+# a = [1, 4, 6, 2, 0, -3]
+# print(a[k - 1])
+
+# Find the number of elements of a list.
+
+# a = [8, 1, 3, -3, 20, -36, 0]
+# print(len(a))
+
+# Reverse a list.
+
+# a = [8, 1, 3, -3, 20, -36, 0]
+# print(a[::-1])
+
+# Find out whether a list is a palindrome.
+
+# a = [9, 8, 6, 8, 9]
+# print(a == a[::-1])
